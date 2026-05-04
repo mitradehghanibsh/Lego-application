a5b39c342872f4ea707a7b838b5602153a2db779
Ermira <ermiraa.zhitiaa@gmail.com>
2026-05-04 15:12:19 +0300

    Update SettingsReader.java

--- src/threads/SettingsReader.java
+++ src/threads/SettingsReader.java
@@ -8,38 +8,53 @@ import data.Robot;
 
 public class SettingsReader implements Runnable {
 
+    @Override
     public void run() {
 
-        while(true) {
+        while (true) {
             try {
 
                 URL url = new URL("http://10.95.162.86:8080/rest/lego/getvalues");
 
                 HttpURLConnection conn =
-                    (HttpURLConnection) url.openConnection();
+                        (HttpURLConnection) url.openConnection();
 
                 conn.setRequestMethod("GET");
+                conn.setConnectTimeout(3000);
+                conn.setReadTimeout(3000);
 
                 BufferedReader br = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream())
+                        new InputStreamReader(conn.getInputStream())
                 );
 
                 String response = br.readLine();
 
-                String[] parts = response.split("#");
+                if (response != null) {
 
-                if(parts.length >= 6) {
+                    String[] parts = response.split("#");
 
-                    Robot.setRun(Integer.parseInt(parts[1]));
-                    Robot.setSpeed(Integer.parseInt(parts[2]));
-                    Robot.setTurn(Integer.parseInt(parts[3]));
-                    Robot.setParkingMode(Integer.parseInt(parts[4]));
-                    Robot.setObstacleDistance(Float.parseFloat(parts[5]));
+                    if (parts.length >= 6) {
+
+                        Robot.setRun(Integer.parseInt(parts[1]));
+                        Robot.setSpeed(Integer.parseInt(parts[2]));
+                        Robot.setTurn(Integer.parseInt(parts[3]));
+                        Robot.setParkingMode(Integer.parseInt(parts[4]));
+                        Robot.setObstacleDistance(Float.parseFloat(parts[5]));
+
+                        // LIVE DEBUG OUTPUT (your feature proof)
+                        System.out.println("Settings updated:");
+                        System.out.println("Run: " + Robot.getRun());
+                        System.out.println("Speed: " + Robot.getSpeed());
+                        System.out.println("Turn: " + Robot.getTurn());
+                        System.out.println("ParkingMode: " + Robot.getParkingMode());
+                        System.out.println("ObstacleDistance: " + Robot.getObstacleDistance());
+                        System.out.println("----------------------");
+                    }
                 }
 
                 Thread.sleep(3000);
 
-            } catch(Exception e) {
+            } catch (Exception e) {
                 System.out.println("Settings read error: " + e.getMessage());
             }
         }
